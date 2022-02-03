d632261bb3b6888c34512bb5e44615333f5ffa06
Lupy29 <96465531+Lupy29@users.noreply.github.com>
2022-02-02 23:09:07 -0800

    Add files via upload

--- index7.js
+++ index7.js
@@ -1,23 +1,55 @@
-//soal 1
-function trafficLight (lamp) {
-switch (lamp) {
-    case "red":
-    return "stop";
-    break;
-  case "yellow":
-    return "heart-heart!";
-    break;
-  case "green":
-    return "go";
-    break;
-}
-}
+// //soal 1
+// function trafficLight (lamp) {
+// switch (lamp) {
+//     case "red":
+//     return "stop";
+//     break;
+//   case "yellow":
+//     return "heart-heart!";
+//     break;
+//   case "green":
+//     return "go";
+//     break;
+// }
+// }
 
-console.log (trafficLight("yellow"))
+// console.log (trafficLight("yellow"))
 
-//soal 2
-const persegi = (sisi) => {
-    return sisi * sisi**2
+// //soal 2
+// const persegi = (sisi) => {
+//     return sisi * sisi**2
+// }
+
+// console.log (persegi(2))
+
+//soal 3
+function array(value) {
+    const result = value.map(el => {
+        const newArr = ""
+        return el += newArr
+    })
+    const split = result[3].split(',')
+    result.pop()
+    console.log(result,split.join(' '))
 }
+array (['hallo','nama','saya',['lucky','fauzi', ['kelas',['11', ['rpl','2']]]]])
 
-console.log (persegi(2))
+//soal 4
+// const obj = {
+//   nama: "lupy fadli m",
+//   kelas: "rpl2",
+//   umur: 17,
+//   nikah: false,
+
+//   Hp: {
+//     merk: "samsung a02s",
+//     spesifikasi: {
+//       chipset: "snapdragon",
+//       ram: "4GB",
+//       rom: "64GB",
+//       camera: "24Mp"
+//     }
+//   }
+// }
+
+// console.log(obj.Hp)
